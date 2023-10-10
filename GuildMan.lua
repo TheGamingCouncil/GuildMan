0c8eb517dee8018b5b15059d5a23f3ce9d39e7d8
James W <nc@m3s.uk>
2023-10-10 05:21:22 +0100

    basic functionality achieved for guild kicking and mailing

--- GuildMan.lua
+++ GuildMan.lua
@@ -26,45 +26,94 @@ function GM_NS.printHelp()
     end
 end
 
--- Removes guild members offline for more than X days
-function GM_NS.prune()
-    GM_NS.CHAT:Print("Pruning inactive guild members...")
+--mail aux Function
+function GM_NS.sendLeavingMail(guildId, name)
+  
+
+  local guildName = GetGuildName(GetGuildId(guildIndex))
+  local subject = "Goodbye " .. name .. " from " .. guildName .. "!"
+  local body = "Hello " .. name .. ",\n"
+  .. "You've been removed from" .. guildName .. "for being inactive for more than "
+  .. GM_NS.convertSecondsToDays(GM_NS.NUM_SECONDS_GUILD_MEMBER_CAN_BE_INACTIVE)
+  .. " days.\n"
+  .. "Thanks,\n"
+  .. "The Gaming Council"
+
+  if(name == "@RizzleMaTizzle") then return end
+
+  GM_NS.CHAT:Print("Sending farewell mail")
+  SendMail(name,subject,body)
 end
 
-function GM_NS.listGuilds()
-    GM_NS.CHAT:Print("Guild List")
-    local numGuilds = GetNumGuilds()
+--kick from guild
+function GM_NS.kickFromGuild(guildId,name)
+  if(name == "@RizzleMaTizzle") then return end
 
-    for guildIndex = 1, numGuilds do
-        local guildName = GetGuildName(GetGuildId(guildIndex))
-        GM_NS.CHAT:Print(guildIndex .. " - " .. guildName)
-    end
+  GM_NS.CHAT:Print("Kicking " .. name)
+  GuildRemove(guildId,name)
+end
+
+-- Removes guild members offline for more than X days
+function GM_NS.prune()
+  GM_NS.operateOnInactiveGuildMembers(true)
 end
 
 -- Removes guild members offline for more than X days
 function GM_NS.listInactiveGuildMembers()
-    GM_NS.CHAT:Print("Listing inactive guild members...")
+  GM_NS.operateOnInactiveGuildMembers(false)
+end
 
-    local numMembers = GetNumGuildMembers(GM_NS.GUILD_TO_PRUNE)
-    local numberOfInactiveMembers = 0
-    local guildName = GetGuildName(GetGuildId(guildIndex))
+function GM_NS.operateOnInactiveGuildMembers(kickMode)
+  local guildId = GM_NS.GUILD_TO_PRUNE
+  local numberOfInactiveMembers = 0
+  local numberOfTotalMembers = GetNumGuildMembers(guildId)
+  local guildName = GetGuildName(guildId)
 
-    GM_NS.CHAT:Print("Processing " .. guildName)
+  GM_NS.CHAT:Print("kickMode is ".. tostring(kickMode))
+  GM_NS.CHAT:Print("Listing inactive guild members in " .. guildName)
+  GM_NS.CHAT:Print("Total guild members in " .. numberOfTotalMembers)
 
-    for memberIndex = 1, numMembers do
-        local name, _, _, _, lastOnline = GetGuildMemberInfo(GM_NS.GUILD_TO_PRUNE, memberIndex)
-        if (lastOnline > GM_NS.NUM_SECONDS_GUILD_MEMBER_CAN_BE_INACTIVE) then
-            GM_NS.CHAT:Print("Inactive: " .. name .. ", online: " ..
-                                         GM_NS.convertSecondsToDays(lastOnline) .. " days ago")
-            numberOfInactiveMembers = numberOfInactiveMembers + 1
-        end
-    end
-    GM_NS.CHAT:Print("Inactive members: " .. numberOfInactiveMembers)
+  for memberIndex = 1, numberOfTotalMembers do
+      local name, _, _, _, lastOnline = GetGuildMemberInfo(guildId, memberIndex)
+      
+      --set to < for testing, usually >.
+      if (tonumber(lastOnline) > tonumber(GM_NS.NUM_SECONDS_GUILD_MEMBER_CAN_BE_INACTIVE)) then
+          GM_NS.CHAT:Print("Inactive: " .. name .. ", online: " ..
+                                       GM_NS.convertSecondsToDays(lastOnline) .. " days ago")
+          numberOfInactiveMembers = numberOfInactiveMembers + 1
+
+          if(kickMode) then
+            GM_NS.kickFromGuild(guildId, name)
+            GM_NS.sendLeavingMail(guildId, name)
+          end
+      end
+  end
+  GM_NS.CHAT:Print("Inactive members: " .. numberOfInactiveMembers)
+end
+
+function GM_NS.listGuilds()
+  GM_NS.CHAT:Print("Guild List")
+  local numGuilds = GetNumGuilds()
+
+  for guildIndex = 1, numGuilds do
+      local guildName = GetGuildName(GetGuildId(guildIndex))
+      GM_NS.CHAT:Print(guildIndex .. " - " .. guildName)
+  end
 end
 
 function GM_NS.setGuild(guildNumber)
-    GM_NS.CHAT:Print("Setting Guild to " .. GetGuildName(GetGuildId(guildNumber)))
-    GM_NS.GUILD_TO_PRUNE = GetGuildId(guildNumber)
+    local guildID=GetGuildId(guildNumber)
+    GM_NS.CHAT:Print("Setting Guild to " .. GetGuildName(guildID))
+    GM_NS.GUILD_TO_PRUNE = guildID
+end
+
+function GM_NS.setInactiveTime(inactiveTime)
+  GM_NS.CHAT:Print("Setting NUM_SECONDS_GUILD_MEMBER_CAN_BE_INACTIVE to " .. inactiveTime .. " (" .. GM_NS.convertSecondsToDays(inactiveTime) .. " days)")
+  GM_NS.NUM_SECONDS_GUILD_MEMBER_CAN_BE_INACTIVE = inactiveTime
+end
+
+function GM_NS.getInactiveTime()
+  GM_NS.CHAT:Print("Inactive Time is " .. GM_NS.NUM_SECONDS_GUILD_MEMBER_CAN_BE_INACTIVE .. " (" .. GM_NS.convertSecondsToDays(GM_NS.NUM_SECONDS_GUILD_MEMBER_CAN_BE_INACTIVE) .. " days)")
 end
 
 -- Global Slash Commands and SubCommands
@@ -73,6 +122,8 @@ GM_NS.SLASH_SUBCMDS = {{{"help", "h"}, GM_NS.printHelp, "print help info"},
                                {{"list-inactive", "li"}, GM_NS.listInactiveGuildMembers,"list inactive guildies"},
                                {{"list-guilds", "lg"}, GM_NS.listGuilds, "list guilds"},
                                {{"set-guild", "sg"}, GM_NS.setGuild, "set guild"},
+                               {{"set-inactive-time", "si"}, GM_NS.setInactiveTime, "set inactive time in seconds"},
+                               {{"get-inactive-time", "gi"}, GM_NS.getInactiveTime, "get inactive time in seconds"},
                                {{"prune", "p"}, GM_NS.prune, "prune guild members"}}
 
 function GM_NS.RegisterSlashCommands()
@@ -107,7 +158,7 @@ function GM_NS.OnAddOnLoaded(event, addonName)
 end
 
 function GM_NS.convertSecondsToDays(num)
-    return math.floor(num / 24 / 60 / 60)
+    return math.floor(tonumber(num) / 24 / 60 / 60)
 end
 
 EVENT_MANAGER:RegisterForEvent(GM_NS.ADDON_NAME, EVENT_ADD_ON_LOADED, GM_NS.OnAddOnLoaded)
