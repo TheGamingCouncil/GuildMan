4f75391933a889ff35faa7c4f459e07d258903a0
James W <nc@m3s.uk>
2023-10-10 01:16:15 +0100

    refactor

--- GuildMan.lua
+++ GuildMan.lua
@@ -1,177 +1,118 @@
---Define Namespace for Addon
-GuildManAddon = {}
- 
---Set name of addon
-GuildManAddon.name = "GuildMan"
+  --Define Namespace for Addon
+  GuildManAddon = {}
 
---Global Vars
-GuildManAddon.G_NUM_SECONDS_INACTIVE=604800
---Global Messages 
-GuildManAddon.G_STRING_INITIALIZE_MSG="Loaded Addon: GuildMan"
+  --Global Vars
+  GuildManAddon.NAME = "GuildMan"
+  GuildManAddon.NUM_SECONDS_GUILD_MEMBER_CAN_BE_INACTIVE=604800
 
---Global Slash Commands
-GuildManAddon.G_STRING_SLASH_CMD_DESC_GUILDMAN="GuildMan - Prints Help"
-GuildManAddon.G_ARRAY_SLASH_CMD_ALIASES_GUILDMAN={"/gm", "/guildman"}
+  --Global Messages 
+  GuildManAddon.INITIALIZE_MESSAGE="Loaded Addon: GuildMan"
 
---Global Slash SubCommands
-GuildManAddon.G_ARRAY_SLASH_SUBCMD_GUILDMAN_PRUNE_ALIAS={"prune","p"}
-GuildManAddon.G_STRING_SLASH_SUBCMD_GUILDMAN_PRUNE_DESC="prune guild members"
+  --Global Libraries
+  GuildManAddon.LOGGER = LibDebugLogger(GuildManAddon.NAME); --Debug logging
+  GuildManAddon.CHAT = LibChatMessage(GuildManAddon.NAME, "GM"); --Output to chat
+  GuildManAddon.LIB_SLASH_CMDR=LibSlashCommander; --CLI Library
 
-GuildManAddon.G_ARRAY_SLASH_SUBCMD_GUILDMAN_HELP_ALIAS={"help","h"}
-GuildManAddon.G_STRING_SLASH_SUBCMD_GUILDMAN_HELP_DESC="print help"
+  --Functions
 
-GuildManAddon.G_ARRAY_SLASH_SUBCMD_GUILDMAN_LIST_INACTIVE_ALIAS={"list-inactive","li"}
-GuildManAddon.G_STRING_SLASH_SUBCMD_GUILDMAN_LIST_INACTIVE_DESC="list inactive guild members"
+  --Prints the help info
+  function GuildManAddon.printHelp()
+    GuildManAddon.CHAT:Print("Help:")
+    GuildManAddon.CHAT:Print("Type: /gm <command>")
+    GuildManAddon.CHAT:Print("<command> can be:")
 
-GuildManAddon.G_ARRAY_SLASH_SUBCMD_GUILDMAN_LIST_GUILDS_ALIAS={"list-guilds","lg"}
-GuildManAddon.G_STRING_SLASH_SUBCMD_GUILDMAN_LIST_GUILDS_DESC="list guilds"
-
-
---Global Libraries
-GuildManAddon.G_LOGGER = LibDebugLogger(GuildManAddon.name)
-GuildManAddon.G_CHAT = LibChatMessage(GuildManAddon.name, "GM")
-GuildManAddon.G_LIB_SLASH_COMMANDER=LibSlashCommander
-
---Functions
-function GuildManAddon.Initialize()
-  GuildManAddon.G_LOGGER:Info(GuildManAddon.G_STRING_INITIALIZE_MSG)
-  GuildManAddon.G_CHAT:Print(GuildManAddon.G_STRING_INITIALIZE_MSG)
-  GuildManAddon.RegisterSlashCommands()
-end
- 
-function GuildManAddon.OnAddOnLoaded(event, addonName)
-  if addonName == GuildManAddon.name then
-    GuildManAddon.Initialize()
-    EVENT_MANAGER:UnregisterForEvent(GuildManAddon.name, EVENT_ADD_ON_LOADED)
-  end
-end
-
---Generic method to register a slash command with Lib_Slash_Commander 
-function GuildManAddon.RegisterSlashCommand(command, aliases, funcToCall, description)
-  
-  --register the command aliases
-  for _, alias in ipairs(aliases) do
-    command:AddAlias(alias)
-  end
-
-  --register the function
-  command:SetCallback(funcToCall)
-
-  --set the description
-  command:SetDescription(description)
-end
-
-function GuildManAddon.RegisterSlashSubCommand(command, aliases, funcToCall, description)
-  local subcommand = command:RegisterSubCommand()
-  --register the command aliases
-  for _, alias in ipairs(aliases) do
-    subcommand:AddAlias(alias)
-  end
-
-  --register the function
-  subcommand:SetCallback(funcToCall)
-
-  --set the description
-  subcommand:SetDescription(description)
-end
-
-function GuildManAddon.RegisterSlashCommands()
- 
-  local command = GuildManAddon.G_LIB_SLASH_COMMANDER:Register()
-
-  --Register /gm slash command
-  GuildManAddon.RegisterSlashCommand(
-    command,
-    GuildManAddon.G_ARRAY_SLASH_CMD_ALIASES_GUILDMAN,
-    GuildManAddon.printHelp,
-    GuildManAddon.G_STRING_SLASH_CMD_DESC_GUILDMAN
-  )
-  --Finish registering /gm slash command
-
-  --Register Subcommands of /gm
-  GuildManAddon.RegisterSlashSubCommand(
-    command,
-    GuildManAddon.G_ARRAY_SLASH_SUBCMD_GUILDMAN_PRUNE_ALIAS,
-    GuildManAddon.prune,
-    GuildManAddon.G_STRING_SLASH_SUBCMD_GUILDMAN_PRUNE_DESC
-  )
-
-  GuildManAddon.RegisterSlashSubCommand(
-    command,
-    GuildManAddon.G_ARRAY_SLASH_SUBCMD_GUILDMAN_HELP_ALIAS,
-    GuildManAddon.printHelp,
-    GuildManAddon.G_STRING_SLASH_SUBCMD_GUILDMAN_HELP_DESC
-  )
-
-  GuildManAddon.RegisterSlashSubCommand(
-    command,
-    GuildManAddon.G_ARRAY_SLASH_SUBCMD_GUILDMAN_LIST_INACTIVE_ALIAS,
-    GuildManAddon.listInactive,
-    GuildManAddon.G_STRING_SLASH_SUBCMD_GUILDMAN_LIST_INACTIVE_DESC
-  )
-
-  GuildManAddon.RegisterSlashSubCommand(
-    command,
-    GuildManAddon.G_ARRAY_SLASH_SUBCMD_GUILDMAN_LIST_GUILDS_ALIAS,
-    GuildManAddon.listGuilds,
-    GuildManAddon.G_STRING_SLASH_SUBCMD_GUILDMAN_LIST_GUILDS_DESC
-  )
-  --Finish Registering Subcommands
-end
-
---Removes guild members offline for more than X days
-function GuildManAddon.prune()
-  GuildManAddon.G_CHAT:Print("Pruning inactive guild members...")
-end
-
---Removes guild members offline for more than X days
-function GuildManAddon.listInactive()
-  GuildManAddon.G_CHAT:Print("Listing inactive guild members...")
-  local numberOfGuilds = GetNumGuilds()
-
-  for guildIndex = 1, numberOfGuilds do
-    local guildName =  GetGuildName(GetGuildId(guildIndex))
-    local numMembers = GetNumGuildMembers(GetGuildId(guildIndex))
-    local numberOfInactiveMembers = 0
-    GuildManAddon.G_CHAT:Print("Processing " .. guildName )
-
-    for memberIndex = 1, numMembers do
-      local name, _, _, _, lastOnline = GetGuildMemberInfo(GetGuildId(guildIndex), memberIndex)
-      if(lastOnline > GuildManAddon.G_NUM_SECONDS_INACTIVE) then
-        GuildManAddon.G_CHAT:Print("Inactive: " .. name .. ", online: " .. GuildManAddon.convertSecondsToDays(lastOnline) .. " days ago")
-        numberOfInactiveMembers = numberOfInactiveMembers + 1
-      end
+    for _, sub_cmd in ipairs(GuildManAddon.SLASH_SUBCMDS) do
+      GuildManAddon.CHAT:Print(" " .. sub_cmd[1][1] .. " (" .. sub_cmd[1][2] .. ")" .. " - " .. sub_cmd[3])
     end
-    GuildManAddon.G_CHAT:Print("Inactive members: " .. numberOfInactiveMembers)
+    
+    for _, sub_cmd in ipairs(GuildManAddon.SLASH_SUBCMDS) do
+      local s_aliases, s_func, s_desc = table.unpack(sub_cmd)
+    end
   end
-  GuildManAddon.G_CHAT:Print("All Guilds Processed")
-end
-
-function GuildManAddon.listGuilds()
-  GuildManAddon.G_CHAT:Print("Guild List")
-  local numGuilds = GetNumGuilds()
-
-  for i = 1, numGuilds do
-    local guildName = GetGuildName(GetGuildId(i))
-    GuildManAddon.G_CHAT:Print(i .. " - " .. guildName)
+  --Removes guild members offline for more than X days
+  function GuildManAddon.prune()
+    GuildManAddon.CHAT:Print("Pruning inactive guild members...")
   end
-end
 
-function GuildManAddon.setGuild()
-  GuildManAddon.G_CHAT:Print("Setting Guild")
+  function GuildManAddon.listGuilds()
+    GuildManAddon.CHAT:Print("Guild List")
+    local numGuilds = GetNumGuilds()
+
+    for guildIndex = 1, numGuilds do
+      local guildName = GetGuildName(GetGuildId(i))
+      GuildManAddon.CHAT:Print(guildIndex .. " - " .. guildName)
+    end
+  end
+
+  --Removes guild members offline for more than X days
+  function GuildManAddon.listInactiveGuildMembers()
+    GuildManAddon.CHAT:Print("Listing inactive guild members...")
+    local numberOfGuilds = GetNumGuilds()
+
+    for guildIndex = 1, numberOfGuilds do
+      local guildName =  GetGuildName(GetGuildId(guildIndex))
+      local numMembers = GetNumGuildMembers(GetGuildId(guildIndex))
+      local numberOfInactiveMembers = 0
+      GuildManAddon.CHAT:Print("Processing " .. guildName )
+
+      for memberIndex = 1, numMembers do
+        local name, _, _, _, lastOnline = GetGuildMemberInfo(GetGuildId(guildIndex), memberIndex)
+        if(lastOnline > GuildManAddon.NUM_SECONDS_GUILD_MEMBER_CAN_BE_INACTIVE) then
+          GuildManAddon.CHAT:Print("Inactive: " .. name .. ", online: " .. GuildManAddon.convertSecondsToDays(lastOnline) .. " days ago")
+          numberOfInactiveMembers = numberOfInactiveMembers + 1
+        end
+      end
+      GuildManAddon.CHAT:Print("Inactive members: " .. numberOfInactiveMembers)
+    end
+    GuildManAddon.CHAT:Print("All Guilds Processed")
+  end
+
+  --Global Slash Commands and SubCommands
+  GuildManAddon.SLASH_CMD={{"/gm", "/guildman"}, GuildManAddon.printHelp, "print help info"}
+  GuildManAddon.SLASH_SUBCMDS={
+    {{"help","h"}, GuildManAddon.printHelp, "print help info"},
+    {{"list-inactive","li"}, GuildManAddon.listInactiveGuildMembers, "list inactive guildies"},
+    {{"prune","p"}, GuildManAddon.prune, "prune guild members"}
+  }
+
+  --Aux functions
+  function GuildManAddon.setGuild()
+    GuildManAddon.CHAT:Print("Setting Guild")
+  end
+
+  function GuildManAddon.RegisterSlashCommands()
+    --Register Slash Command
+    local command = GuildManAddon.LIB_SLASH_CMDR:Register()
+    command:AddAlias(GuildManAddon.SLASH_CMD[1][1])
+    command:AddAlias(GuildManAddon.SLASH_CMD[1][2])
+    command:SetCallback(GuildManAddon.SLASH_CMD[2])
+    command:SetDescription(GuildManAddon.SLASH_CMD[3])
+
+    -- Register Slash Subcommands
+    for _, sub_cmd in ipairs(GuildManAddon.SLASH_SUBCMDS) do
+      local subcommand = command:RegisterSubCommand()
+      subcommand:AddAlias(sub_cmd[1][1])
+      subcommand:AddAlias(sub_cmd[1][2])
+      subcommand:SetCallback(sub_cmd[2])
+      subcommand:SetDescription(sub_cmd[3])
+    end
+  end
+
+  function GuildManAddon.Initialize()
+    GuildManAddon.LOGGER:Info(GuildManAddon.INITIALIZE_MESSAGE)
+    GuildManAddon.CHAT:Print(GuildManAddon.INITIALIZE_MESSAGE)
+    GuildManAddon.RegisterSlashCommands()
+  end
   
-end
+  function GuildManAddon.OnAddOnLoaded(event, addonName)
+    if addonName == GuildManAddon.NAME then
+      GuildManAddon.Initialize()
+      EVENT_MANAGER:UnregisterForEvent(GuildManAddon.NAME, EVENT_ADD_ON_LOADED)
+    end
+  end
 
---Prints the help info
-function GuildManAddon.printHelp()
-  GuildManAddon.G_CHAT:Print("valid commands: \n/gm \n/gm prune \n/gm list-inactive")
-end
+  function GuildManAddon.convertSecondsToDays(num)
+    return math.floor(num / 24 / 60 / 60)
+  end
 
-function GuildManAddon.convertSecondsToDays(num)
-  return math.floor(num / 24 / 60 / 60)
-end
-
-
- 
-EVENT_MANAGER:RegisterForEvent(GuildManAddon.name, EVENT_ADD_ON_LOADED, GuildManAddon.OnAddOnLoaded)
-
+  EVENT_MANAGER:RegisterForEvent(GuildManAddon.NAME, EVENT_ADD_ON_LOADED, GuildManAddon.OnAddOnLoaded)
